8391d72e674d9af80b9bedfb6cad8218245037de
DhanyDaVinci <dendyardany@gmail.com>
2020-11-07 17:28:04 +0700

    test page

--- application/views/public/dashboard/dashboard_main.php
+++ application/views/public/dashboard/dashboard_main.php
@@ -14,56 +14,6 @@
                                 </div>
                             </div>
                            
-                        <?php 
-                        /*
-                        $dir    = 'files/djarum_id/campaign_report/';
-                        $files1 = array_diff(scandir($dir,1), array('..', '.'));
-                        
-                        echo '<pre>';
-                        print_r($files1[0]);
-                        echo '</pre>';
-                        */
-                        //unlink('files/djarum_id/campaign_report/Djarum_Version_Report_20201102035217923.csv');
-                            foreach ($collection as $al){
-                        ?>
-                            <div class="col-md-4 center-block">
-                                <div class="card">
-                                    <div class="header_purple">
-                                        <center>
-                                            
-                                        <p class="tittle_box"><?php echo $al['account_list'][0];?></p>
-                                        </center>
-                                    </div>
-                                    <div class="card-body">
-                                        <small>Table prefix : <?=$al['prefix'][0]?></small><br>
-                                        <table class="table table-bordered mt-8">
-                                          <tbody>
-                                            
-                                            <tr>
-                                              <td>Campaign Report</td>
-                                              <td><?php echo number_format($al['tbl_rec']['camp_report']['t_record']);?> &nbsp;records</td>
-                                            </tr>
-                                            <tr>
-                                              <td>Campaign Version</td>
-                                              <td><?php echo number_format($al['tbl_rec']['version_report']['t_record']);?> &nbsp;records</td>
-                                            </tr>
-                                            <tr>
-                                              <td>Campaign Unique</td>
-                                              <td><?php echo number_format($al['tbl_rec']['unique_report']['t_record']);?> &nbsp;records</td>
-                                            </tr>
-                                            <tr>
-                                              <td>Campaign Video</td>
-                                              <td><?php echo number_format($al['tbl_rec']['video_report']['t_record']);?> &nbsp;records</td>
-                                            </tr>
-                                              
-                                          </tbody>
-                                        </table>
-<!--                                        <small>Last update : November, 5 2020</small>-->
-                                    </div>
-                                </div>
-                            </div>
-                        
-                        <? } ?>
                         
                         </div>
                     
